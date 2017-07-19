455caa1dda87b533e580eb3ef433d23d65227bd3
john.revill <john.revill@digital.hmrc.gov.uk>
2017-07-19 15:44:24 +0100

    Some initial repository code for looking up business rates by postcode

---
+++ app/uk/gov/hmrc/agentdemofrontend/repository/BusinessRatesRepository.scala
@@ -0,0 +1,26 @@
+package uk.gov.hmrc.agentdemofrontend.repository
+
+import play.api.libs.json.Json
+import play.modules.reactivemongo.MongoDbConnection
+import reactivemongo.api.{DB, ReadPreference}
+import reactivemongo.api.commands.WriteResult
+import reactivemongo.api.indexes.{Index, IndexType}
+import reactivemongo.bson.BSONObjectID
+import uk.gov.hmrc.agentdemofrontend.model.BusinessRate
+import uk.gov.hmrc.mongo.json.ReactiveMongoFormats
+import uk.gov.hmrc.mongo.{ReactiveRepository, Repository}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+trait BusinessRatesRepository {
+  def findBusinessRates(postCode: String): Future[Seq[BusinessRate]]
+}
+
+class BusinessRatesMongoRepository(mongo: () => DB)
+  extends ReactiveRepository[BusinessRate, BSONObjectID]("businessrates", mongo, BusinessRate.format, ReactiveMongoFormats.objectIdFormats)
+    with BusinessRatesRepository with Repository[BusinessRate, BSONObjectID] {
+  override def findBusinessRates(postCode: String): Future[Seq[BusinessRate]] = {
+    collection.find(Json.obj("postcode" -> postCode)).cursor[BusinessRate](ReadPreference.primary).collect[List]()
+  }
+}
